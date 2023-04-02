cd00a2c39f007094cfb484f50879fddae8200d1f
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-04-02 13:20:32 +0300

    Update задание 1.py

--- задание 1.py
+++ задание 1.py
@@ -2,7 +2,7 @@ x1=float(input())
 x2=float(input())
 y1=float(input())
 y2=float(input())
-def can_eat(x1,x2,y1,y2):
+def can_eat(x1,x2,y1,y2):#Конь съедает фигуру если разность по x 2 и по y 1 и наоборот и мы просто проверяем по условию
     if x1-x2==2 and y1-y2==1:
         return True
     elif x1-x2==1 and y1-y2==2:
