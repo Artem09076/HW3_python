a65f79e116123f07f3d054d287d2fb1f349075e6
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-04-02 12:15:22 +0300

    Add files via upload

---
+++ задание 1.py
@@ -0,0 +1,14 @@
+x1=float(input())
+x2=float(input())
+y1=float(input())
+y2=float(input())
+def can_eat(x1,x2,y1,y2):
+    if x1-x2==2 and y1-y2==1:
+        return True
+    elif x1-x2==1 and y1-y2==2:
+        return True
+    else:
+        return False
+
+print(can_eat(x1,x2,y1,y2))
+    
