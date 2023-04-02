c25363070368726dda49b64e5f204b4519ef75e7
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-04-02 13:40:35 +0300

    Update задание 6.py

--- задание 6.py
+++ задание 6.py
@@ -1,22 +1,22 @@
 n= int(input())
 matrix=[]
 
-for i in range(n): #запрос n
+for i in range(n): 
 
-    row = input().split(' ') #каждая строка разделяется на элементы
+    row = input().split(' ') #строка разделяется на элементы
 
     if len(row) == n:
 
-        matrix.append(row) #если кол-во элементов равно n, то строка добавляется в список a
+        matrix.append(row) #если количество элементов равно n, то строка добавляется в список 
 
 
 def IsSymmetric(matrix,n):
         for j in range(n):
-            if matrix[i][j] != matrix[j][i]:
+            if matrix[i][j] != matrix[j][i]:#Если числа с индексами i и j не равны симметричным то по логике массив не симметричный
                 return False
         return True
 
-if  IsSymmetric(matrix,n):
+if  IsSymmetric(matrix,n):#Здесь если функция вывела True то пишем No если False то No
     print("YES")
 else:
     print("NO")
